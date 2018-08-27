290d838149a926f0c589b4ebb57ca45b07770897
riceFun <ricefun@ricefundeimac.local>
2018-08-27 10:48:21 +0800

    Initial Commit

---
+++ UIResponderRouterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIResponderRouterDemo
+//
+//  Created by riceFun on 2018/8/27.
+//  Copyright © 2018年 riceFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIResponderRouterDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIResponderRouterDemo
+//
+//  Created by riceFun on 2018/8/27.
+//  Copyright © 2018年 riceFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
